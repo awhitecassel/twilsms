c6ac09ebc2c5d45dbd1fa773bb931c89d34cf2d4
Anthony Cassel <awhitecassel@gmail.com>
2016-02-23 11:34:08 -0500

    Update twilsms.rb

--- twilsms.rb
+++ twilsms.rb
@@ -1,13 +1,13 @@
 require 'twilio-ruby'
 
-accountSID = 'AC86d81fd14193dd3847570f52e822db2b'
-authtoken = '09ca34fc1d9b120c9953326feffc237f'
+accountSID = 
+authtoken = 
 
 @client = Twilio::REST::Client.new accountSID, authToken
 
 from = '+12677336304'
 
-friends = { '+12673745180' => "Anthony Cassel"}
+friends = { '' => "Anthony Cassel"}
 
 friends.each do |key, value|
 	message = @client.account.message.create(
